4d118257c47b33a6a17d0972d3261a08cbb87b5c
Denis Megerle <megerle.denis@gmail.com>
2020-07-24 16:02:52 +0200

    added orthogonal weight and const bias initialization

--- cfg.py
+++ cfg.py
@@ -1,5 +1,5 @@
 
-from networks import _simple_actor_net, _simple_critic_net, _super_simple_actor_net, _super_simple_critic_net, _twolayer_mlp_actor_net, _twolayer_mlp_critic_net, _twolayer_mlp_policy_net, _twolayer_complex_mlp_policy_net
+from networks import _twolayer_mlp_actor_net, _twolayer_mlp_critic_net, _twolayer_mlp_actor_net_orth, _twolayer_mlp_critic_net_orth
 from ContCartpoalEnv import ContinuousCartPoleEnv
 from ReachingDotEnv import ReachingDotEnv
 import gym
@@ -7,208 +7,13 @@ import os, sys
 from _utils import RolloutInverseTimeDecay
 
 from tensorflow.keras.optimizers.schedules import InverseTimeDecay
-#from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
 
 #sys.path.append('../SimulationFramework/simulation/src/gym_envs//mujoco/')
 #from gym_envs.mujoco.reach_env import ReachEnv
 
-"""
-[summary]
-
-"""
-
-cont_ppo_cartpoal_cfg = {
-  # ACTOR
-  'actor_model' : _simple_actor_net,
-  'alpha_actor' : 1e-4,             # learning rate actor
-  'actor_epochs' : 8,               #
-  'actor_batchsize' : 32,
-  'actor_shuffle' : False,
-  
-  # CRITIC
-  'critic_model' : _simple_critic_net,
-  'alpha_critic' : 1e-4,            # learning rate critic
-  'critic_epochs' : 8,
-  'critic_shuffle' : True,
-  'critic_batchsize' : 32,
-  'qval_clip' : 0.2,
-  
-  'gae_gamma' : 0.99,               # reward discount factor
-  'gae_lambda' : 0.95,              # smoothing for advantage, reducing variance in training
-  
-  'ppo_clip' : 0.2,                 # clipping value of ppo
-  'ppo_entropy_factor' : 0.001,     # entropy factor according to ppo paper
-  
-  # ACTOR_TRAINING
-  
-  
-  # ENVIRONMENT / TRAINING
-  'environment' : ContinuousCartPoleEnv(),
-  'num_stab' : 1e-10,   # value for numeric stabilization of div/log
-
-  'rollout_episodes' : 5,
-  'total_episodes' : 20000,
-  'print_intervall' : 20,
-  'rollout_steps' : 64
-}
-
-cont_ppo_testnormal_cfg = {
-  # ACTOR
-  'actor_model' : _twolayer_mlp_actor_net,
-  'alpha_actor' : 1e-4,             # learning rate actor
-  'actor_epochs' : 8,               #
-  'actor_batchsize' : 32,
-  'actor_shuffle' : False,
-  
-  # CRITIC
-  'critic_model' : _twolayer_mlp_critic_net,
-  'alpha_critic' : 1e-4,            # learning rate critic
-  'critic_epochs' : 8,
-  'critic_shuffle' : False,
-  'critic_batchsize' : 32,
-  'qval_clip' : 0.2,
-  
-  'gae_gamma' : 0.99,               # reward discount factor
-  'gae_lambda' : 0.95,              # smoothing for advantage, reducing variance in training
-  
-  'ppo_clip' : 0.2,                 # clipping value of ppo
-  'ppo_entropy_factor' : 0.001,     # entropy factor according to ppo paper
-  
-  # ACTOR_TRAINING
-  
-  
-  # ENVIRONMENT / TRAINING
-  'environment' : ContinuousCartPoleEnv(),
-  #'environment' : VecNormalize(DummyVecEnv([lambda: ContinuousCartPoleEnv()]), norm_obs=True, norm_reward=True,
-  #                 clip_obs=10.),
-  #'environment' : ReachEnv(max_steps=500, control='ik', render=False, randomize_objects=False),
-  'num_stab' : 1e-10,   # value for numeric stabilization of div/log
-
-  'rollout_episodes' : 5,
-  'total_episodes' : 5000,
-  'print_interval' : 20,
-}
-
-cont_ppo_test_cartpole_cfg = {
-  # ACTOR
-  'actor_model' : _twolayer_mlp_policy_net,
-  'alpha_actor' : 1e-4,             # learning rate actor
-  'actor_epochs' : 10,               #
-  'actor_batchsize' : 64,
-  'actor_shuffle' : False,
-  
-  # CRITIC
-  'alpha_critic' : 3e-4,            # learning rate critic
-  'critic_epochs' : 5,
-  'critic_shuffle' : False,
-  'critic_batchsize' : 64,
-  'vest_clip' : 0.2,
-  
-  'gae_gamma' : 0.99,               # reward discount factor
-  'gae_lambda' : 0.95,              # smoothing for advantage, reducing variance in training
-  
-  'ppo_clip' : 0.2,                 # clipping value of ppo
-  'ppo_entropy_factor' : 0.001,     # entropy factor according to ppo paper
-  
-  # ACTOR_TRAINING
-  
-  
-  # ENVIRONMENT / TRAINING
-  #'environment' : ReachEnv(max_steps=2048, control='ik', render=True, randomize_objects=False),
-  'environment' : ContinuousCartPoleEnv(seed=1),
-  #'environment' : VecNormalize(DummyVecEnv([lambda: ContinuousCartPoleEnv(seed=1)]), norm_obs=True, norm_reward=True,
-  #                 clip_obs=10.),
-  'num_stab' : 1e-10,   # value for numeric stabilization of div/log
-
-  'rollout_episodes' : 5,
-  'total_episodes' : 5000,
-  'print_interval' : 20,
-  'rollout_steps' : 64,
-  
-  'total_steps' : 1000000,
-  'rollout' : 2048
-}
-
-cont_ppo_test_shared_cfg = {
-  # ACTOR
-  'actor_model' : _twolayer_mlp_policy_net,
-  'alpha_actor' : 3e-4,             # learning rate actor
-  'actor_epochs' : 10,               # 5 for other envs
-  'actor_batchsize' : 64,
-  'actor_shuffle' : False,
-  
-  # CRITIC
-  'vest_clip' : 0.2,
-  
-  'gae_gamma' : 0.99,               # reward discount factor
-  'gae_lambda' : 0.95,              # smoothing for advantage, reducing variance in training
-  
-  'ppo_clip' : 0.2,                 # clipping value of ppo
-  'ppo_entropy_factor' : 0.000,     # entropy factor according to ppo paper
-  'value_loss_factor' : 0.5,        # factor for value loss
-  # ACTOR_TRAINING
-  'normalize_advantages' : False,
-  
-  # ENVIRONMENT / TRAINING
-  #'environment' : ReachEnv(control='ik', render=True, randomize_objects=False),
-  'environment' : ContinuousCartPoleEnv(seed=1),
-  #'environment' : gym.make('HalfCheetah-v2'),
-  #'environment' : ReachingDotEnv(seed=1),
-  #'environment' : VecNormalize(DummyVecEnv([lambda: ContinuousCartPoleEnv(seed=1)]), norm_obs=True, norm_reward=True,
-  #                 clip_obs=10.),
-  'num_stab' : 1e-10,   # value for numeric stabilization of div/log
-
-  'rollout_episodes' : 5,
-  'total_episodes' : 5000,
-  'print_interval' : 20,
-  'rollout_steps' : 64,
-  
-  'total_steps' : 1000000,
-  'rollout' : 2048
-}
-cont_ppo_test_reachenv_cfg = {
-  # ACTOR
-  'actor_model' : _twolayer_complex_mlp_policy_net,
-  'alpha_actor' : 3e-4,             # learning rate actor
-  'actor_epochs' : 10,               #
-  'actor_batchsize' : 64,
-  'actor_shuffle' : False,
-  
-  # CRITIC
-  'alpha_critic' : 3e-4,            # learning rate critic
-  'critic_epochs' : 5,
-  'critic_shuffle' : False,
-  'critic_batchsize' : 64,
-  'vest_clip' : 0.2,
-  
-  'gae_gamma' : 0.99,               # reward discount factor
-  'gae_lambda' : 0.95,              # smoothing for advantage, reducing variance in training
-  
-  'ppo_clip' : 0.2,                 # clipping value of ppo
-  'ppo_entropy_factor' : 0.001,     # entropy factor according to ppo paper
-  
-  # ACTOR_TRAINING
-  
-  
-  # ENVIRONMENT / TRAINING
-  #'environment' : ReachEnv(render=True),
-  #'environment' : ContinuousCartPoleEnv(seed=1),
-  #'environment' : VecNormalize(DummyVecEnv([lambda: ReachEnv(render=True)]), norm_obs=True, norm_reward=True,
-  #                 clip_obs=10.),
-  'num_stab' : 1e-10,   # value for numeric stabilization of div/log
-
-  'rollout_episodes' : 5,
-  'total_episodes' : 5000,
-  'print_interval' : 20,
-  'rollout_steps' : 64,
-  
-  'total_steps' : 1000000,
-  'rollout' : 2048
-}
-
 cont_ppo_test_split_cfg = {
   # ACTOR
-  'actor_model' : _twolayer_mlp_actor_net,
+  'actor_model' : _twolayer_mlp_actor_net_orth,
   'adam_actor_alpha' : RolloutInverseTimeDecay(3e-4, 10000, 0.0, staircase=True),             # learning rate actor
   'actor_epochs' : 3,               # 5
   'actor_batchsize' : 64,
@@ -216,7 +21,7 @@ cont_ppo_test_split_cfg = {
   'adam_actor_epsilon' : 1e-5,
   
   # CRITIC
-  'critic_model' : _twolayer_mlp_critic_net,
+  'critic_model' : _twolayer_mlp_critic_net_orth,
   'adam_critic_alpha' : RolloutInverseTimeDecay(3e-4, 10000, 0.0, staircase=True),            # learning rate critic
   'critic_epochs' : 10,
   'critic_shuffle' : False,
@@ -234,7 +39,7 @@ cont_ppo_test_split_cfg = {
   'value_loss_factor' : 1.0,        # factor for value loss
   
   # ACTOR_TRAINING
-  'normalize_advantages' : True,     # minibatch advantage normalization
+  'normalize_advantages' : False,     # minibatch advantage normalization
   'normalize_observations' : True,   # running mean + variance normalization
   'normalize_rewards' : True,        # running variance normalization
   'scale_actions' : True,
@@ -244,7 +49,7 @@ cont_ppo_test_split_cfg = {
   
   # ENVIRONMENT / TRAINING
   #'environment' : ReachEnv(control='ik', render=True, randomize_objects=False),
-  'environment' : (lambda : ReachingDotEnv(seed=777)),
+  'environment' : (lambda : ContinuousCartPoleEnv(seed=777)),
   #'environment' : gym.make('HalfCheetah-v2'),
   #'environment' : ReachingDotEnv(seed=1),
   #'environment' : VecNormalize(DummyVecEnv([lambda: ContinuousCartPoleEnv(seed=1)]), norm_obs=True, norm_reward=True,
--- networks.py
+++ networks.py
@@ -1,91 +1,70 @@
+import numpy as np
+
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.initializers import Constant, Orthogonal
 
-def _simple_actor_net(input_dim, output_dim):
-  state = Input(shape=input_dim)
+orth_weights_initializer = lambda scale, seed=1 : Orthogonal(gain=scale, seed=seed)
+const_bias_initializer = lambda value : Constant(value=value)
 
-  dense1 = Dense(64, activation='relu')(state)
-  dense2 = Dense(64, activation='relu')(dense1)
-  dense3 = Dense(32, activation='relu')(dense2)
-  action_mu = Dense(output_dim, activation='tanh')(dense3)
-  action_sig = Dense(output_dim, activation='softplus')(dense3)
-
-  return Model(inputs=state, outputs=[action_mu, action_sig])
-
-def _simple_critic_net(input_dim):
-  state = Input(shape=input_dim)
-    
-  dense1 = Dense(64, activation='relu')(state)
-  dense2 = Dense(64, activation='relu')(dense1)
-  dense3 = Dense(32, activation='relu')(dense2)
-  q_val = Dense(1, activation='tanh')(dense3)
-    
-  model = Model(inputs=state, outputs=q_val)
-  return model
-
-def _twolayer_mlp_policy_net(input_dim, output_dim):
-  state = Input(shape=input_dim)
-
-  dense1 = Dense(200, activation='tanh')(state)
-  dense2 = Dense(100, activation='tanh')(dense1)
-  dense3 = Dense(64, activation='tanh')(dense2)
-  action_mu = Dense(output_dim, activation='tanh')(dense2)
-  action_sig = Dense(output_dim, activation='softplus')(dense2)
-  v_est = Dense(1, activation='linear')(dense3)
-
-  return Model(inputs=state, outputs=[action_mu, action_sig, v_est])
-
-def _twolayer_complex_mlp_policy_net(input_dim, output_dim):
-  state = Input(shape=input_dim)
-
-  dense1 = Dense(128, activation='tanh')(state)
-  dense2 = Dense(128, activation='tanh')(dense1)
-  dense3 = Dense(64, activation='tanh')(dense2)
-  action_mu = Dense(output_dim, activation='tanh')(dense2)
-  action_sig = Dense(output_dim, activation='softplus')(dense2)
-  v_est = Dense(1, activation='tanh')(dense3)
-
-  return Model(inputs=state, outputs=[action_mu, action_sig, v_est])
-
-
-
+# TODO think about normalization after denses
 
 def _twolayer_mlp_actor_net(input_dim, output_dim):
   state = Input(shape=input_dim)
 
-  dense1 = Dense(64, activation='tanh')(state)
-  dense2 = Dense(64, activation='tanh')(dense1)
-  action_mu = Dense(output_dim, activation='tanh')(dense2)
-  action_sig = Dense(output_dim, activation='softplus')(dense2)
+  dense1 = Dense(64,  activation='tanh')(state)
+  dense2 = Dense(64,  activation='tanh')(dense1)
+
+  action_mu = Dense(output_dim,   activation='tanh')(dense2)
+  action_sig = Dense(output_dim,  activation='softplus')(dense2)
 
   return Model(inputs=state, outputs=[action_mu, action_sig])
 
 def _twolayer_mlp_critic_net(input_dim, output_dim):
   state = Input(shape=input_dim)
     
-  dense1 = Dense(64, activation='relu')(state)
-  dense2 = Dense(64, activation='relu')(dense1)
-  v_est = Dense(output_dim, activation=None)(dense2)
+  dense1 = Dense(64,  activation='tanh', 
+                      kernel_initializer=orth_weights_initializer(np.sqrt(2)), 
+                      bias_initializer=const_bias_initializer(0.0))(state)
+  dense2 = Dense(64,  activation='tanh', 
+                      kernel_initializer=orth_weights_initializer(np.sqrt(2)), 
+                      bias_initializer=const_bias_initializer(0.0))(dense1)
+
+  v_est = Dense(output_dim,   activation=None)(dense2)
   
   return Model(inputs=state, outputs=v_est)
 
-
-
-
-def _super_simple_actor_net(input_dim, output_dim):
+def _twolayer_mlp_actor_net_orth(input_dim, output_dim):
   state = Input(shape=input_dim)
 
-  dense1 = Dense(64, activation='relu')(state)
-  action_mu = Dense(output_dim, activation='tanh')(dense1)
-  action_sig = Dense(output_dim, activation='softplus')(dense1)
+  dense1 = Dense(64,  activation='tanh', 
+                      kernel_initializer=orth_weights_initializer(np.sqrt(2)), 
+                      bias_initializer=const_bias_initializer(0.0))(state)
+  dense2 = Dense(64,  activation='tanh', 
+                      kernel_initializer=orth_weights_initializer(np.sqrt(2)), 
+                      bias_initializer=const_bias_initializer(0.0))(dense1)
+
+  action_mu = Dense(output_dim,   activation='tanh', 
+                                  kernel_initializer=orth_weights_initializer(0.01), 
+                                  bias_initializer=const_bias_initializer(0.0))(dense2)
+  action_sig = Dense(output_dim,  activation='softplus',
+                                  kernel_initializer=orth_weights_initializer(np.sqrt(2)), 
+                                  bias_initializer=const_bias_initializer(0.0))(dense2)
 
   return Model(inputs=state, outputs=[action_mu, action_sig])
 
-def _super_simple_critic_net(input_dim):
+def _twolayer_mlp_critic_net_orth(input_dim, output_dim):
   state = Input(shape=input_dim)
     
-  dense1 = Dense(64, activation='relu')(state)
-  q_val = Dense(1, activation='tanh')(dense1)
-    
-  model = Model(inputs=state, outputs=q_val)
-  return model
+  dense1 = Dense(64,  activation='tanh', 
+                      kernel_initializer=orth_weights_initializer(np.sqrt(2)), 
+                      bias_initializer=const_bias_initializer(0.0))(state)
+  dense2 = Dense(64,  activation='tanh', 
+                      kernel_initializer=orth_weights_initializer(np.sqrt(2)), 
+                      bias_initializer=const_bias_initializer(0.0))(dense1)
+
+  v_est = Dense(output_dim,   activation=None,
+                              kernel_initializer=orth_weights_initializer(1.0), 
+                              bias_initializer=const_bias_initializer(0.0))(dense2)
+  
+  return Model(inputs=state, outputs=v_est)
