fixed bug when using the simplified normalization wrapper env, now also printing the rewards per episode unnormalized

ContinuousAgent.py
@@ -15,7 +15,7 @@ from scipy.signal import lfilter
 import pickle as pkl
 from scipy import io as scio
 from itertools import accumulate
-from _utils import npscanr, NormalizeWrapper
+from _utils import npscanr, NormalizeWrapper, RunningMeanStd
 import datetime
 
 
@@ -24,14 +24,14 @@ class Agent(object):
   
   def __init__(self, cfg):
     self.cfg = cfg
-    self.env = NormalizeWrapper(self.cfg['environment'], 
+    self.env = NormalizeWrapper(self.cfg['environment'],
                                 norm_obs=self.cfg['normalize_observations'], norm_reward=self.cfg['normalize_rewards'],
-                                clip_obs=10., clip_reward=10., 
-                                gamma=0.99, epsilon=1e-8)
+                                clip_obs=self.cfg['clip_observations'], clip_reward=self.cfg['clip_rewards'],
+                                gamma=self.cfg['gamma_env_normalization'], epsilon=self.cfg['num_stab_envnorm'])
     
     self.input_dim = self.env.observation_space.shape
     self.n_actions = self.env.action_space.shape[0]
-
+    
     self.action_space_means = (self.env.action_space.high + self.env.action_space.low) / 2.0
     self.action_space_magnitude = (self.env.action_space.high - self.env.action_space.low) / 2.0
     
@@ -114,11 +114,11 @@ class Agent(object):
     var = tf.cast(K.square(sig), tf.float32)
     denom = tf.cast(K.sqrt(2 * math.pi * var), tf.float32)
     deviation = tf.cast(K.square(x - mu), tf.float32)
-    expon = - (1 / 2) * deviation / (var + self.cfg['num_stab'])
+    expon = - (1 / 2) * deviation / (var + self.cfg['num_stab_pdf'])
     return (1 / denom) * K.exp(expon)
   
   def _ppo_clip_loss(self, pi_new, pi_old, advantage):
-    ratio = pi_new / (pi_old + self.cfg['num_stab'])
+    ratio = pi_new / (pi_old + self.cfg['num_stab_ppo'])
     clip_ratio = K.clip(ratio, min_value=1 - self.cfg['ppo_clip'], max_value=1 + self.cfg['ppo_clip'])
 
     surrogate1 = ratio * advantage
@@ -135,7 +135,7 @@ class Agent(object):
     return K.mean(K.minimum(surrogate1, surrogate2))
     
   def _entropy_norm_pdf(self, sig):
-    entropy_loss = K.log(K.sqrt(2 * math.pi * math.e * K.square(sig)) + self.cfg['num_stab'])
+    entropy_loss = K.log(K.sqrt(2 * math.pi * math.e * K.square(sig)) + self.cfg['num_stab_pdf'])
     return - K.mean(entropy_loss)
   
   def _train(self, returns, advantages):
@@ -166,7 +166,7 @@ class Agent(object):
         batch_action_sig = [x[1] for x in batch_action_dist]
         
         if self.cfg['normalize_advantages']:
-          batch_advantage = (batch_advantage - batch_advantage.mean()) / np.maximum(batch_advantage.std(), self.cfg['num_stab'])
+          batch_advantage = (batch_advantage - batch_advantage.mean()) / np.maximum(batch_advantage.std(), self.cfg['num_stab_advnorm'])
         
         with tf.GradientTape(persistent=True) as tape:
           batch_y_pred_mu, batch_y_pred_sig = self.actor(batch_states)
@@ -196,7 +196,7 @@ class Agent(object):
         
         gradient = tape.gradient(critic_loss, self.critic.trainable_variables)
         self.critic_optimizer.apply_gradients(zip(gradient, self.critic.trainable_variables))
-        
+  
   def train(self):
     # calculate returns and advantages
     returns, advantages = self._calculate_gae(self.v_est_memory, self.reward_memory, self.not_done_memory)
@@ -214,7 +214,7 @@ class Agent(object):
       # self.env.render()
       scaled_a, unscaled_a, a_dist = self.actor_choose(s)   # sa=scaled, ua=unscaled
       s_, r, done, _ = self.env.step(scaled_a)
-      ep_score += r
+      ep_score += self.env.unnormalize_reward(r)
       v_est = self.critic_evaluate(s)
       self.store_transition(s, unscaled_a, a_dist, r, v_est, not done)
       s = s_

_utils.py
@@ -40,18 +40,19 @@ class NormalizeWrapper(VecNormalize):
     return super().reset()[0]
   
   def step(self, action):
-    return [x[0] for x in super().step(action)]
+    return [x[0] for x in super().step([action])]
 
 
 class RunningMeanStd(object):
   
-  def __init__(self):
+  def __init__(self, shape=()):
+    self.shape = shape
     self.reset()
   
   def reset(self):
     self.k = 0
-    self.M_k = 0
-    self.S_k = 0
+    self.M_k = np.zeros(self.shape)
+    self.S_k = np.zeros(self.shape)
   
   def update(self, x):
     self.k += 1
@@ -61,4 +62,19 @@ class RunningMeanStd(object):
     
   def __call__(self, x):
     self.update(x)
-    return self.M_k, self.S_k / self.k
+    
+    if self.k == 1:
+      return self.M_k, np.ones(self.shape)
+    else: return self.M_k, self.S_k / self.k
+
+
+  # def _normalize_observations(self, observations, not_dones):
+  #   def func(obs, not_done):
+  #     mean, variance = self.obs_rms(obs)
+  #     if not not_done: self.obs_rms.reset()
+  #     return mean, variance
+    
+  #   mean_vars = map(func, observations, not_dones)
+  #   normalized_observations = map(lambda obs, m_v: (obs - m_v[0]) / np.sqrt(m_v[1] + 1e-4), observations, mean_vars)
+    
+  #   return list(normalized_observations)